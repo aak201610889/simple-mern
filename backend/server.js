f3b9ceba57e8a5505728c2de128d111cda38f369
ayham-kattan <ayhamkattan1@gmail.com>
2021-12-03 11:19:18 +0200

    first-commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose=require('mongoose')
+require('dotenv').config();
+const app = express();
+const port = process.env.port || 5000;
+app.use(cors());
+app.use(express.json())
+//connect to database
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true })
+
+
+app.get('/',(req,res)=>{res.send('hello')})
+app.listen(port ,()=>{console.log('server on port '+port);})
