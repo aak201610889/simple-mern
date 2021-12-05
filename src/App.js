74c5ddd39db44d33f4999781f1767142ec537678
ayham-kattan <ayhamkattan1@gmail.com>
2021-12-05 15:02:36 +0200

    clean-react-1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import './App.css';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import ExercisesList from './components/ExercisesList'
+import EditExercise from './components/EditExercise'
+import CreateExercise from './components/CreateExercise'
+import CreateUser from "./components/CreateUser";
 function App() {
   return (
-    <div className="App">
-      <header>
-        react
-      </header>
-      <main>
-main
-      </main>
-      <footer>
-footer
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Link to="/">ExercisesList</Link>
+          <Link to="/edit/:id">EditExercise</Link>
+          <Link to="/create">CreateExercise</Link>
+          <Link to="/user">CreateUser</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<ExercisesList/>} />
+            <Route path="/edit/:id" element={<EditExercise/>} />
+            <Route path="/create" element={<CreateExercise/>} />
+            <Route path="/user" element={<CreateUser/>} />
+          </Routes>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
